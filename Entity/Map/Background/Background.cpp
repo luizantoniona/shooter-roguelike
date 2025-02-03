a1927b531641e89e11307d6027abdd268d21a968
luizantoniona <luizantonio.na@hotmail.com>
2025-02-03 13:16:58 -0300

    feat: create class for map background

---
+++ Entity/Map/Background/Background.cpp
@@ -0,0 +1,11 @@
+#include "Background.h"
+
+BEGIN_ENTITY_NAMESPACE
+
+Background::Background() {
+}
+
+Background::~Background() {
+}
+
+END_ENTITY_NAMESPACE
---
+++ Entity/Map/Background/Background.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Entity/EntityGlobals.h>
+
+BEGIN_ENTITY_NAMESPACE
+
+class Background {
+public:
+    Background();
+    ~Background();
+
+private:
+};
+
+END_ENTITY_NAMESPACE
