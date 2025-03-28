96c737c2a91a281941808da92a390ac350457890
luizantoniona <luizantonio.na@hotmail.com>
2025-03-28 16:34:03 -0300

    feat: Implement basic bar component

--- GUI/Bar/Bar.cpp
+++ GUI/Bar/Bar.cpp
@@ -2,13 +2,26 @@
 
 BEGIN_GUI_NAMESPACE
 
-Bar::Bar() {
+Bar::Bar( float width, float height, sf::Color backgroundColor, sf::Color fillColor ) :
+    _value( 1.0f ) {
+    _background.setSize( { width, height } );
+    _background.setFillColor( backgroundColor );
+
+    _fill.setSize( { width, height } );
+    _fill.setFillColor( fillColor );
 }
 
 void Bar::update( sf::Time& deltaTime ) {
 }
 
 void Bar::render( sf::RenderWindow& window ) {
+    window.draw( _background );
+    window.draw( _fill );
+}
+
+void Bar::setValue( float value ) {
+    _value = std::clamp( value, 0.0f, 1.0f );
+    _fill.setSize( { _background.getSize().x * _value, _background.getSize().y } );
 }
 
 END_GUI_NAMESPACE
--- GUI/Bar/Bar.h
+++ GUI/Bar/Bar.h
@@ -10,10 +10,17 @@ BEGIN_GUI_NAMESPACE
 
 class Bar : public Component {
 public:
-    Bar();
+    Bar( float width, float height, sf::Color backgroundColor, sf::Color fillColor );
 
     void update( sf::Time& deltaTime ) override;
     void render( sf::RenderWindow& window ) override;
+
+    void setValue( float value );
+
+protected:
+    float _value;
+    sf::RectangleShape _background;
+    sf::RectangleShape _fill;
 };
 
 END_GUI_NAMESPACE
