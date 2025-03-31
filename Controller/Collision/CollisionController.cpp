b8ae4707ef8c0cce8b07dc04668aac01da457da8
luizantoniona <luizantonio.na@hotmail.com>
2025-03-31 17:42:28 -0300

    feat: Detect player vs enemies collision

--- Controller/Collision/CollisionController.cpp
+++ Controller/Collision/CollisionController.cpp
@@ -3,22 +3,49 @@
 BEGIN_CONTROLLER_NAMESPACE
 
 void CollisionController::checkCollisions( Player& player, std::vector<std::unique_ptr<Enemy>>& enemies ) {
+    handleProjectileCollisions( player, enemies );
+    handlePlayerCollisions( player, enemies );
+}
+
+void CollisionController::handleProjectileCollisions( Player& player, std::vector<std::unique_ptr<Enemy>>& enemies ) {
     auto& projectiles = player.getProjectiles();
-    for ( auto it = projectiles.begin(); it != projectiles.end(); ) {
+
+    for ( auto projectileIt = projectiles.begin(); projectileIt != projectiles.end(); ) {
         bool hit = false;
+
         for ( auto enemyIt = enemies.begin(); enemyIt != enemies.end(); ) {
-            if ( enemyIt->get()->getShape().getGlobalBounds().intersects( it->getGlobalBounds() ) ) {
+            if ( enemyIt->get()->getShape().getGlobalBounds().intersects( projectileIt->getGlobalBounds() ) ) {
+
                 enemyIt = enemies.erase( enemyIt );
+
                 hit = true;
                 break;
+
             } else {
                 ++enemyIt;
             }
         }
+
         if ( hit ) {
-            it = projectiles.erase( it );
+            projectileIt = projectiles.erase( projectileIt );
+
         } else {
-            ++it;
+            ++projectileIt;
+        }
+    }
+}
+
+void CollisionController::handlePlayerCollisions( Player& player, std::vector<std::unique_ptr<Enemy>>& enemies ) {
+    for ( auto enemyIt = enemies.begin(); enemyIt != enemies.end(); ) {
+        if ( enemyIt->get()->getShape().getGlobalBounds().intersects( player.getShape().getGlobalBounds() ) ) {
+
+            auto& playerStatus = player.getPlayerStatus();
+            int currentHealth = playerStatus.getHealth();
+
+            playerStatus.setHealth( currentHealth - enemyIt->get()->getAttack() );
+
+        } else {
+            ++enemyIt;
         }
     }
 }
--- Controller/Collision/CollisionController.h
+++ Controller/Collision/CollisionController.h
@@ -15,6 +15,10 @@ BEGIN_CONTROLLER_NAMESPACE
 class CollisionController {
 public:
     void checkCollisions( Player& player, std::vector<std::unique_ptr<Enemy>>& enemies );
+
+private:
+    void handleProjectileCollisions( Player& player, std::vector<std::unique_ptr<Enemy>>& enemies );
+    void handlePlayerCollisions( Player& player, std::vector<std::unique_ptr<Enemy>>& enemies );
 };
 
 END_CONTROLLER_NAMESPACE
