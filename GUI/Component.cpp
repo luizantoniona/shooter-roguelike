66b3829beb4b1bbfb4825d623b55e93727a16304
luizantoniona <luizantonio.na@hotmail.com>
2025-03-31 17:41:33 -0300

    feat: Every component need a name

--- GUI/Component.cpp
+++ GUI/Component.cpp
@@ -2,4 +2,12 @@
 
 BEGIN_GUI_NAMESPACE
 
+Component::Component( std::string componentName ) :
+    _componentName( componentName ) {
+}
+
+std::string Component::getComponentName() const {
+    return _componentName;
+}
+
 END_GUI_NAMESPACE
--- GUI/Component.h
+++ GUI/Component.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include <SFML/Graphics.hpp>
 
 #include <GUI/GUIGlobals.h>
@@ -8,13 +10,16 @@ BEGIN_GUI_NAMESPACE
 
 class Component {
 public:
+    Component( std::string componentName );
     virtual ~Component() = default;
 
     virtual void update( sf::Time& deltaTime ) = 0;
     virtual void render( sf::RenderWindow& window ) = 0;
 
-protected:
-    sf::Vector2f _position;
+    std::string getComponentName() const;
+
+private:
+    std::string _componentName;
 };
 
 END_GUI_NAMESPACE
