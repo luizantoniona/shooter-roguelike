c589e355a674061090ffd55ddf070240d8866d07
luizantoniona <luizantonio.na@hotmail.com>
2025-04-30 17:43:35 -0300

    fix: Fixing pointer types

--- Controller/Game/Player/Projectile/PlayerProjectileController.cpp
+++ Controller/Game/Player/Projectile/PlayerProjectileController.cpp
@@ -8,8 +8,8 @@ void PlayerProjectileController::updatePlayerProjectile( const sf::Time& deltaTi
     auto& projectiles = player.getProjectiles();
 
     for ( auto& projectile : projectiles ) {
-        sf::Vector2f movement = projectile.getDirection() * projectile.getSpeed() * deltaTime.asSeconds();
-        projectile.getShape().move( movement );
+        sf::Vector2f movement = projectile->getDirection() * projectile->getSpeed() * deltaTime.asSeconds();
+        projectile->getShape()->move( movement );
     }
 
     projectiles.erase(
@@ -26,7 +26,7 @@ void PlayerProjectileController::firePlayerProjectile( const sf::Vector2f& targe
     auto& projectileStatus = player.getProjectileStatus();
 
     if ( player.getFireClock().getElapsedTime().asSeconds() >= ( 1.0f / projectileStatus.getProjectileFireRate() ) ) {
-        sf::Vector2f direction = target - player.getShape().getPosition();
+        sf::Vector2f direction = target - player.getShape()->getPosition();
         float length = std::hypot( direction.x, direction.y );
 
         if ( length > 0.0f ) {
@@ -36,10 +36,10 @@ void PlayerProjectileController::firePlayerProjectile( const sf::Vector2f& targe
             projectile.setDirection( direction );
             projectile.setSpeed( projectileStatus.getProjectileSpeed() );
             projectile.setDamage( projectileStatus.getProjectileDamage() );
-            projectile.getShape().setSides( 3 );
-            projectile.getShape().setPosition( player.getShape().getPosition() );
-            projectile.getShape().setRadius( projectileStatus.getProjectileSize() );
-            projectile.getShape().build();
+            projectile.getShape()->setSides( 3 ); // TODO: Como definir o número de sides? Trocar por circulo?
+            projectile.getShape()->setPosition( player.getShape()->getPosition() );
+            projectile.getShape()->setSize( projectileStatus.getProjectileSize() );
+            projectile.getShape()->build();
 
             player.getProjectiles().emplace_back( projectile );
             player.getFireClock().restart();
