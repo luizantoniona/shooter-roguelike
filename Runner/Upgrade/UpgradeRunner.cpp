b44e68347b0bb5f4d4c248b603af2195266d3539
luizantoniona <luizantonio.na@hotmail.com>
2025-06-17 17:38:17 -0300

    feat: exit upgraderunner on exit

--- Runner/Upgrade/UpgradeRunner.cpp
+++ Runner/Upgrade/UpgradeRunner.cpp
@@ -3,19 +3,20 @@
 BEGIN_RUNNER_NAMESPACE
 
 UpgradeRunner::UpgradeRunner() :
-    Runner() {
-}
+    Runner() {}
 
-UpgradeRunner::~UpgradeRunner() {
-}
+UpgradeRunner::~UpgradeRunner() {}
 
 void UpgradeRunner::handleInput( sf::RenderWindow& window, const sf::Event& event, const sf::Time& deltaTime ) {
+
+    if ( event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape ) {
+        _runnerCallback( RunnerType::MENU );
+    }
+
 }
 
-void UpgradeRunner::update( sf::RenderWindow& window, const sf::Time& deltaTime ) {
-}
+void UpgradeRunner::update( sf::RenderWindow& window, const sf::Time& deltaTime ) {}
 
-void UpgradeRunner::render( sf::RenderWindow& window ) {
-}
+void UpgradeRunner::render( sf::RenderWindow& window ) {}
 
 END_RUNNER_NAMESPACE
